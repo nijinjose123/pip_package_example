a58ef79861ff55bcbf8d7e2c03d062098a8251d8
Nijin THykkathu <nijin.thykkathu@stfc.ac.uk>
2018-08-22 16:12:34 +0100

    .

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='piptutorial',
       description='The funniest job in the world',
       url= 'https://github.com/nijinjose123/pip_package_example.git',
       author='Nijin Thykkathu',
-      author_email='nijin.thykkathu@stfc.ac.uk',
+      author_email='nijinjose123@gmail.com',
       packages=['piptutorial'],
       install_requires=[
           'markdown'
